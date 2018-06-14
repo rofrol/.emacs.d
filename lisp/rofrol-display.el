a05cd1f9d800a0953d87a54697be6033a6a3f55a
Roman Frołow <rofrol@gmail.com>
2018-06-14 15:14:55 +0200

    Toggle menu-bar, tool-bar, maximized instead of fullscreen

--- lisp/rofrol-display.el
+++ lisp/rofrol-display.el
@@ -7,8 +7,8 @@
       inhibit-startup-message t
       initial-scratch-message "")
 
-;;; http://ergoemacs.org/emacs/emacs_line_number_mode.html
-;;; https://emacs.stackexchange.com/questions/36747/disable-line-numbers-in-helm-buffers-emacs-26
+;; http://ergoemacs.org/emacs/emacs_line_number_mode.html
+;; https://emacs.stackexchange.com/questions/36747/disable-line-numbers-in-helm-buffers-emacs-26
 (when (version<= "26.0.50" emacs-version )
       ;;; enable in all programming modes
       (defun init-line-numbers-mode ()
@@ -18,10 +18,15 @@
 
 (column-number-mode t)
 
-;;; https://emacs.stackexchange.com/questions/2999/how-to-maximize-my-emacs-frame-on-start-up/3017#3017
-;;; https://www.emacswiki.org/emacs/FullScreen
-;;; faster startup without resizing: emacs -fs
+;; https://emacs.stackexchange.com/questions/2999/how-to-maximize-my-emacs-frame-on-start-up/3017#3017
+;; https://www.emacswiki.org/emacs/FullScreen
+;; faster startup without resizing: emacs -fs
+;; if you want maximized, don't use -mm on Windows
+;; looks like toggle-frame-maximized is faster than default-frame-alist
+;; https://emacs.stackexchange.com/questions/2999/how-to-maximize-my-emacs-frame-on-start-up/3000#3000
+(toggle-frame-maximized)
 (add-to-list 'default-frame-alist '(fullscreen . maximized))
+;;(add-to-list 'initial-frame-alist '(fullscreen . maximized))
 ;;(add-hook 'window-setup-hook 'toggle-frame-fullscreen t)
 (global-set-key [f11] 'toggle-frame-fullscreen)
 
@@ -49,12 +54,33 @@
     :init
     (progn
       (defun init-treemacs ()
-        (treemacs)
+        ;;(treemacs)
         ;;; set focus to file
         ;;; http://emacsredux.com/blog/2013/04/28/switch-to-previous-buffer/
         (switch-to-buffer (other-buffer (current-buffer) 1))
         (global-set-key [f8] 'treemacs))
 
       (add-hook 'emacs-startup-hook 'init-treemacs)))
+
+;;(tool-bar-mode -1)
+;;(menu-bar-mode -1)
+;; faster start: emacsclientw.exe -c -n -a "runemacs -fs --xrm \"Emacs.toolBar:0\" --xrm \"Emacs.menuBar:0\""
+
+;;(defun toggle-bars ()
+;;    (interactive)
+;;    (toggle-menu-bar-mode-from-frame)
+;;    (toggle-tool-bar-mode-from-frame))
+
+;; works only once
+;;(global-set-key [f7] 'toggle-bars)
+
+;; works only once
+;; https://unix.stackexchange.com/questions/7280/in-gnu-emacs-how-do-i-set-up-a-global-key-to-toggle-the-menu-bar/7291#7291
+;;(defun toggle-menu-toolbar-modes ()
+;;  (interactive)
+;;  (tool-bar-mode (menu-bar-mode)))
+;;(global-set-key (kbd "<f5>") 'toggle-menu-toolbar-modes)
+(global-set-key (kbd "<f6>") 'toggle-menu-bar-mode-from-frame)
+(global-set-key (kbd "<f7>") 'toggle-tool-bar-mode-from-frame)
 
 (provide 'rofrol-display)
