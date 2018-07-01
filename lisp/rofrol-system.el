b50288223be1643cac28153520d19fc1a6114804
Roman Frołow <rofrol@gmail.com>
2018-07-01 12:33:33 +0200

    Do not insert pair when looking at space, tab or eol

--- lisp/rofrol-system.el
+++ lisp/rofrol-system.el
@@ -19,6 +19,13 @@
 ;; http://ergoemacs.org/emacs/emacs_insert_brackets_by_pair.html
 (electric-pair-mode t)
 
+;; https://emacs.stackexchange.com/questions/12269/getting-electric-pair-mode-to-behave-more-nicely-around-existing-strings/14836#14836
+(setq-default electric-pair-inhibit-predicate
+              (lambda (c)
+                (if (looking-at "\\([ \t]\\|$\\)")
+                    (electric-pair-default-inhibit c)
+                    t)))
+
 (defalias 'yes-or-no-p 'y-or-n-p)
 
 (provide 'rofrol-system)
