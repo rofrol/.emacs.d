9fdf89f1f7c34c5fed0023bc9c355fc3025e39dd
Roman Frołow <rofrol@gmail.com>
2018-08-16 16:14:59 +0200

    Enable lexical-scoping. Paramteterize bury-compile-buffer-if-successful

--- init.el
+++ init.el
@@ -1,3 +1,5 @@
+;; -*- lexical-binding: t; -*-
+
 ;; https://github.com/lewang/flx
 (setq gc-cons-threshold 200000000)
 
@@ -892,17 +894,21 @@ return nil if path is a file"
     (message "There is no buffer named \"*Occur*\".")))
 
 
+;; require library cl at compile time, to get the use of its macros (and not get any runtime load). That is where macro lexical-let is defined. https://emacs.stackexchange.com/questions/15189/alternative-to-lexical-let/15191#15191
+;; (eval-when-compile (require 'cl))
+
 ;; https://emacs.stackexchange.com/questions/62/hide-compilation-window
-(defun bury-compile-buffer-if-successful-elm (buf desc)
-  (if (and
-       (null (string-match ".*exited abnormally.*" desc))
-       (not
-        (with-current-buffer buf
-          (search-forward "WARNING" nil t))))
-      ;;no errors, make the compilation window go away in a few seconds
-      (progn
-        (run-at-time
-         "2 sec" nil 'delete-windows-on
-         (get-buffer-create "*elm-make*"))
-        (message "No Compilation Errors!"))))
+(defun bury-compile-buffer-if-successful (name warning)
+  (lambda(buf desc)
+    (if (and (null (string-match ".*exited abnormally.*" desc))
+	     (not (with-current-buffer buf (search-forward warning nil t))))
+	;;no errors, make the compilation window go away in a few seconds
+	(progn
+	  (run-at-time "2 sec" nil 'delete-windows-on (get-buffer-create name))
+	  (message "No Compilation Errors!")))))
+
+;; https://emacs.stackexchange.com/questions/32544/function-that-takes-a-function-as-argument-and-returns-a-new-function#comment50322_32546
+(fset 'bury-compile-buffer-if-successful-elm
+      (bury-compile-buffer-if-successful "*elm-make*" "WARNING"))
+
 (add-hook 'compilation-finish-functions 'bury-compile-buffer-if-successful-elm)
