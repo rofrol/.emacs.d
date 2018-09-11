8282ae7869e6d90983b621595993a92b9a2f7eb2
Roman Frołow <rofrol@gmail.com>
2018-09-11 13:14:36 +0200

    evil-escape

--- init.el
+++ init.el
@@ -956,5 +956,26 @@ return nil if path is a file"
   ;; (setq evil-split-window-below t)
   ;; (setq evil-shift-round nil)
   ;; (setq evil-want-C-u-scroll t)
+  (setq evil-want-fine-undo t) 
   :config ;; tweak evil after loading it
   (evil-mode))
+
+;; https://github.com/syl20bnr/spacemacs/issues/9330#issuecomment-319448299
+;; http://wikemacs.org/wiki/Evil
+(use-package evil-escape
+ :straight t
+ :after evil
+ :commands evil-escape-mode
+ :init
+ (setq evil-escape-key-sequence "jk"
+       evil-escape-delay 0.25)
+ (evil-escape-mode)
+ :config
+   (define-key evil-normal-state-map "\C-y" 'yank)
+   (define-key evil-insert-state-map "\C-y" 'yank)
+   (define-key evil-visual-state-map "\C-y" 'yank)
+   (define-key evil-insert-state-map "\C-e" 'end-of-line)
+   (define-key evil-normal-state-map "\C-w" 'evil-delete)
+   (define-key evil-insert-state-map "\C-w" 'evil-delete)
+   (define-key evil-insert-state-map "\C-r" 'search-backward)
+   (define-key evil-visual-state-map "\C-w" 'evil-delete))
