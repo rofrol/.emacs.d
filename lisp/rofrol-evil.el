28646d6432ad201241e8a11aaf98b113a4edc4d9
Roman Frołow <rofrol@gmail.com>
2019-02-12 13:09:39 +0100

    set split direction for evil

--- lisp/rofrol-evil.el
+++ lisp/rofrol-evil.el
@@ -14,8 +14,8 @@
   :init ;; tweak evil's configuration before loading it
   ;; (setq evil-search-module 'evil-search)
   ;; (setq evil-ex-complete-emacs-commands nil)
-  ;; (setq evil-vsplit-window-right t)
-  ;; (setq evil-split-window-below t)
+  (setq evil-vsplit-window-right t)
+  (setq evil-split-window-below t)
   ;; (setq evil-shift-round nil)
   ;; (setq evil-want-C-u-scroll t)
   (setq evil-want-fine-undo t) 
