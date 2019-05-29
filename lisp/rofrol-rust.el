fa1a3fc616b13a6d440f33962c3dfe65672b241d
Roman Frołow <rofrol@gmail.com>
2019-05-30 00:53:23 +0200

    format rust code without jumping to beginning when doing undo

--- lisp/rofrol-rust.el
+++ lisp/rofrol-rust.el
@@ -4,6 +4,18 @@
     :mode ("\\.lalrpop\\'" . elm-mode)
     :straight t
     :config
-	(setq rust-format-on-save t))
+    (setq rust-format-on-save nil)
+    (defun rust-mode-format-buffer ()
+      "format rust code without jumping to beginning when doing undo"
+      "https://github.com/rust-lang/rust-mode/issues/162#issuecomment-497137206"
+      (interactive)
+      (shell-command
+       (format "rustfmt %s"
+	       (shell-quote-argument (buffer-file-name))))
+      (revert-buffer t t t))
+
+    (add-hook 'rust-mode-hook
+	      (lambda ()
+		(add-hook 'after-save-hook #'rust-mode-format-buffer nil 'make-it-local))))
 
 (provide 'rofrol-rust)
